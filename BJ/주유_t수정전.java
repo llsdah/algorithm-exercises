a58f29b763e2be429116b066288120e2df1ba87a
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-05-12 15:58:17 +0900

    Create 주유_t수정전.java

---
+++ BJ/주유_t수정전.java
@@ -0,0 +1,71 @@
+package backJun;
+
+import java.io.InputStreamReader;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Scanner;
+
+public class B_test {
+
+	public static void main(String[] args) {
+/*
+		Scanner scan = new Scanner(new InputStreamReader(System.in));
+
+		int n = scan.nextInt();
+		int[] dis = new int[n-1];
+		int[] cost = new int[n];
+
+		for (int i = 0; i < n-1; i++) {
+			dis[i] = scan.nextInt();
+		}
+		
+		for (int i = 0; i < n; i++) {
+			cost[i] = scan.nextInt();
+		}
+		*/
+		// 제일 왼쪽에서 오른쪾으로 이동합니다. 하핳
+		
+		int n=4;
+		int[] dis = {3,3,4};
+		int[] cost = {1,1,1,1};
+		
+		
+		int sum =0;
+		for(int i =0;i<cost.length-1;i++) {
+			int nums =cost[i];
+			int distance =0;
+			for(int k=i; k<cost.length-1;k++) {
+				if(nums<=cost[k]) {
+					distance+=dis[k];
+				}else {
+					sum+=(distance*nums);
+					distance=0;
+					i=k-1;
+					break;
+				}
+			}
+			
+			if(distance!=0) {
+				sum+=(distance*nums);
+				break;
+			}
+		}
+
+		System.out.println(sum);
+		
+	}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
