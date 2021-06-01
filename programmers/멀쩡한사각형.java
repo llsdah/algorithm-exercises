5517e215f7189ba53720d48464e07cf82ec66b40
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-06-01 12:55:33 +0900

    Update 멀쩡한사각형.java
    범위 변환.. 큰수면..ㅠㅠ

--- programmers/멀쩡한사각형.java
+++ programmers/멀쩡한사각형.java
@@ -4,16 +4,20 @@ class Solution {
         //3 3 = 3 같으면 그 갯숫 만큼 입니다. 
         // 3 2 = 4 
         // 5 4 = 
-        if(w==h) return w;
-        // 그림 그려서 생각 해보자 
-        int gcd= gcd ( w,h);
+        // 범위 값 확인 필수 !!! 
+        long wid = Long.valueOf(w);
+        long hei = Long.valueOf(h);
+        
+        if(wid==hei) return wid*hei-wid;
+        // 그림 그려서 생각 해보자
+        long gcd= gcd ( wid,hei);
         // 대각선의 최대공약수 까지 나눈수 있다 
         // 직선의 갯수는 최소한의 거리입니다. 
         
-        answer = w*h-(w/gcd+h/gcd-1)*gcd;
+        answer = wid*hei-wid-hei+gcd;
         return answer;
     }
-    public int gcd(int w,int h){
+    public long gcd(long w,long h){
         if(w%h==0){
             return h;
         }else {
@@ -21,3 +25,16 @@ class Solution {
         }
     }
 }
+/*
+class Solution {
+    public long solution(long w, long h) {
+        long answer = 0;
+        
+        for(int i=0; i<w; i++) {
+            answer += h*i/w ;
+        }
+        
+        return answer * 2;
+    }
+}
+*/
