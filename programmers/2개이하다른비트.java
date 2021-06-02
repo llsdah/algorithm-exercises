3d5b99778abc83f5a40febeb7ea390e699a0d0fb
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-06-02 09:41:26 +0900

    Update 2개이하다른비트.java

--- programmers/2개이하다른비트.java
+++ programmers/2개이하다른비트.java
@@ -41,3 +41,56 @@ class Solution {
         return str.toCharArray();
     }
 }
+
+/*
+정답!!
+import java.util.*;
+class Solution {
+    public long[] solution(long[] numbers) {
+        int leng = numbers.length;
+        long[] answer = new long[leng];
+        for(int i =0; i< leng;i++){
+            if(numbers[i]%2==0){
+                answer[i] = numbers[i]+1;
+                continue;
+            }
+            char[] st = bi(numbers[i]);
+            answer[i] = cnt(st);
+        }
+        return answer;
+    }
+    public long cnt(char[] st){
+        int pos = 0;
+        for(int i =0; i<st.length;i++){
+            if(st[i]=='0'){
+                pos =i;// 자리 저장 
+                st[i]='1';
+                break;
+            }
+        }
+        for(int i =pos-1;i>=0;i-- ){
+            if(st[i]=='1'){
+                st[i]='0';
+                break;
+            }
+        }
+        long num =0;
+        for(int i=0; i<st.length;i++){
+            num+= ((st[i]-'0')*Math.pow(2,i));
+        }
+        //System.out.println(Arrays.toString(st));
+        return num; 
+    }
+    
+    public char[] bi(long num){
+        StringBuilder sb = new StringBuilder();
+        while(num > 0 ){
+            sb.append(num%2);
+            num=num/2;
+        }
+        String str = sb.toString()+"0"; 
+        return str.toCharArray();
+    }
+}
+
+*/
