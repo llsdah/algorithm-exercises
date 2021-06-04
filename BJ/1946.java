1e94b6327e2840e6b031b614d4d4b2d03b300b06
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-06-04 14:25:49 +0900

    Create 1946.java
    왜자꾸 시간 초과가 발썡하지... 같은 코드 쓴사람들은 통과라던데

---
+++ BJ/1946.java
@@ -0,0 +1,91 @@
+public class Main {
+
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(new InputStreamReader(System.in));
+
+		int n = scan.nextInt();
+
+		for (int i = 0; i < n; i++) {
+			int per = scan.nextInt();
+			int[][] arr = new int[per][2];
+
+			for (int k = 0; k < per; k++) {
+				arr[k][0] = scan.nextInt(); //서류
+				arr[k][1] = scan.nextInt();//면접 
+			}
+			
+			Arrays.sort(arr, (o1, o2) -> Integer.compare(o1[1], o2[1]));
+			int cnt = 1;
+			int st = arr[0][0]; // 첫번쨰 기준
+			// 기준
+			for (int h = 1; h < arr.length; h++) {
+				if (arr[h][0] < st) {
+					st = arr[h][0];
+					cnt++;
+				}
+
+			}
+
+			System.out.println(cnt);
+		}
+
+	}
+
+}
+
+
+
+
+
+
+
+/*
+
+public class Main {
+	static int cnt ;
+	public static void main(String[] args) {
+
+		Scanner scan = new Scanner(new InputStreamReader(System.in));
+
+		int n =scan.nextInt();
+		int[] ans = new int[n];
+		for(int i =0; i<n;i++) {
+			int per = scan.nextInt();
+			int[][] arr = new int[per][2];
+			cnt =per;
+			for(int k=0; k<per;k++) {
+				arr[k][0] = scan.nextInt();
+				arr[k][1] = scan.nextInt();
+			}
+			int temp = rank(arr);
+			ans[i]=temp;
+		}
+		for(int i=0; i< ans.length;i++) {
+			System.out.print(ans[i]+" ");
+		}
+		
+		
+	}
+	public static int rank(int[][] arr ) {
+		Arrays.sort(arr, (o1,o2)->Integer.compare(o1[1], o2[1]));
+
+		// 기준
+		for(int i =1; i< arr.length;i++) {
+			for(int k=0; k<i;k++) {
+				if(arr[i][0]>arr[k][0]) {
+					//System.out.println("arr[i][0]>arr[k][0]"+arr[i][0]+"  "+arr[k][0]);
+					cnt--;
+					break;
+				}
+			}
+			
+		}
+		
+		return cnt;
+	}
+}
+
+
+
+*/
