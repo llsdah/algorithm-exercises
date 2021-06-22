622480cc5a5a03c67115ec8b3baa3c78437fdf69
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-06-22 15:21:03 +0900

    여행경로 수정
    ArrayList

--- programmers/여행경로_(수정).java
+++ programmers/여행경로_(수정).java
@@ -1,3 +1,56 @@
+
+/* 답 1
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Date;
+
+public class A_Test {
+
+	public static void main(String[] args) {
+
+
+		String[][] a = {{"ICN", "SFO"}, {"ICN", "ATL"}, {"SFO", "ATL"}, {"ATL", "ICN"}, {"ATL","SFO"}};
+		String[] result = solution(a);
+		
+		
+		System.out.println("result : " + Arrays.toString(result));
+	}
+	static boolean[] visited;                    //방문한지 안한지를 체크하는 visited배열
+    static ArrayList<String> answers;
+ 
+    public static String[] solution(String[][] tickets) {
+        visited = new boolean[tickets.length];    
+        answers = new ArrayList<String>();
+        int count = 0;                                    //몇개의 공항을 들렸는지 알려주는 카운트
+        dfs(count, "ICN", "ICN",tickets);                
+        
+        
+        Collections.sort(answers);                        //답들 중 가장 알파벳순이 빠른 배열들로 정렬
+        String[] answer = answers.get(0).split(" ");    //가장 빠른 배열을 뽑아서 String형 배열로 만듬
+        return answer;
+    }
+    
+    public static void dfs(int count, String present, String answer, String[][]ticktes) {
+        if(count == ticktes.length) {            //모든 공항을 들렸다면
+            answers.add(answer);                //answers에 추가
+            return;
+        }
+        for(int i = 0; i < ticktes.length; i++) {
+            if(!visited[i] && ticktes[i][0].equals(present)) {        //present와 같고 들리지 않은 공항을 찾고
+                visited[i] = true;                                    //해당 공항을 들린걸로 만듬.
+                dfs(count+1, ticktes[i][1],answer+" "+ticktes[i][1] , ticktes);    //카운트 +1 도착 공항을 present로 넣어주고 answer에 도착공항을 추가해준다.
+                visited[i] = false;
+            }
+        }
+        return;
+    }
+
+}
+*/
+/*
 import java.util.*;
 
 class Solution {
@@ -88,3 +141,5 @@ class City {
     }
     
 }
+
+*/
