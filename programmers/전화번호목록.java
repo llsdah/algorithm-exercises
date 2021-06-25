af7a808d9b57b3a84dbe2af1b905f19fec7bed6d
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-06-25 14:54:21 +0900

    Update 전화번호목록.java
    시간초과 부분 수정.

--- programmers/전화번호목록.java
+++ programmers/전화번호목록.java
@@ -1,10 +1,9 @@
 class Solution {
     public boolean solution(String[] phone_book) {
         boolean answer = true;
-        for(int i=0; i<phone_book.length;i++){
+        for(int i=0; i<phone_book.length-1;i++){
             String str = phone_book[i];
-            for(int k=0;k<phone_book.length;k++){
-                if(i==k) continue;
+            for(int k=i+1;k<phone_book.length;k++){
                 if(phone_book[k].startsWith(str)) return false;
                 if(str.startsWith(phone_book[k])) return false;
             }
