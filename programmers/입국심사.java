3ae63bcf701b28ee74adaa9d8bb389fef8459748
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-06-25 09:20:10 +0900

    입국심사(수정_이탐)
    long 계산 주의!! int+int

--- programmers/입국심사.java
+++ programmers/입국심사.java
@@ -1,5 +1,33 @@
 import java.util.*;
 class Solution {
+    // 이분 탐색 최대 시간 최소간 정한다. -> 중간 시간 정함. -> 그 시간동안 모든 사람이 검사 가능한지 체크, 시간이 남으면 줄이고, 적으면 늘린다. 
+    public long solution(int n, int[] times) {
+        long answer = 0;
+        Arrays.sort(times);
+        long st = 0; // 시작 시간
+        long end = (long)n*times[times.length-1];
+        long mid = 0;
+        while(st <= end){ // 교차되면 종료...
+            mid =(st+end)/2;
+            long sum = 0 ; // 몇명 검사 했는지 
+            for(int i =0; i<times.length;i++){ // 빨리 심사하는 순 (이미 정렬)
+               sum+=(mid/times[i]); 
+            }
+            if(sum<n) {
+                st =mid+1; 
+           }// 적은 인원수 처리
+            else { 
+                end = mid-1;
+                 answer=mid;
+            } // 더 많이 처리 하거나 같거나 . 하하하 . 같은 경우 떄문에 여기에 ans = mid가 같도록 
+                   
+        }       
+        return answer;
+    }
+}
+/*
+import java.util.*;
+class Solution {
     // 정렬 한다. -> 깊은 복사 -> 각 사람의 수만큼 돌면서 최소값과 같아 지는 경우 해당 값에 그 값을 더한다.  
     // 최초의 사람은 무조건 들어간다 생각 그래서 여기는 것들이 변화 안함.. 그러나 갯수가 많아 탐색이 안됨. 
     public long solution(int n, int[] times) {
@@ -25,3 +53,4 @@ class Solution {
         return answer;
     }
 }
+*/
