1e0f41b9dd6693e62723c335d5899f448a27cf48
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-06-29 19:32:28 +0900

    Create HIndex.java

---
+++ programmers/HIndex.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] citations) {
+        int answer = 0;
+        Arrays.sort(citations);
+        for(int i =1; i<=citations.length;i++){
+            if(citations[citations.length-i]>=i){
+                answer = i;
+            }
+        }
+        return answer;
+    }
+}
