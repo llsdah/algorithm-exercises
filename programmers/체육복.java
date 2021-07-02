6da38a372d43954b560e2665329f11f86daca8e8
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-07-02 11:09:38 +0900

    Update 체육복.java

--- programmers/체육복.java
+++ programmers/체육복.java
@@ -2,6 +2,43 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[] lost, int[] reserve) {
         int answer = 0;
+        int[] nums= new int[n+2];
+        Arrays.fill(nums,1);
+        nums[0]=0;
+        nums[n+1]=0;
+        // 남은거 한개씩 더해 주기 
+        for(int i=0;i<reserve.length;i++){
+            nums[reserve[i]]++;
+        }
+        //없으면 스스로 뺴기 ..
+        for(int i=0; i<lost.length;i++){
+            nums[lost[i]]--;
+        }
+        System.out.println(Arrays.toString(nums));
+        // 옆에 있는거 부터 2개 이상이면 뻇끼 
+        for(int i=0; i<lost.length;i++){
+            if(nums[lost[i]]>=1){
+                continue;
+            }else if(nums[lost[i]-1]>=2) { // 앞사람에게 빌리
+                nums[lost[i]-1]--;
+                nums[lost[i]]++;
+            }else if( nums[lost[i]+1]>=2){ // 뒤사람 빌리
+                nums[lost[i]+1]--;
+                nums[lost[i]]++;
+            }
+        }
+        System.out.println(Arrays.toString(nums));
+        answer = (int)Arrays.stream(nums).filter(i -> i>=1).count();
+        
+        return answer;
+    }
+}
+
+/* 옛날 꺼 . 
+import java.util.*;
+class Solution {
+    public int solution(int n, int[] lost, int[] reserve) {
+        int answer = 0;
         Arrays.sort(lost);
         Arrays.sort(reserve);
         int[] cnt = new int[n+1];
@@ -43,3 +80,6 @@ class Solution {
         return answer;
     }
 }
+
+
+*/
