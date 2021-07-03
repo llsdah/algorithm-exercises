aa9bc82d6f2ad3b82b909683e0098593f416928f
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-07-03 11:31:10 +0900

    Update and rename 조이싀틱.java to 조이스틱.java

--- programmers/조이스틱.java
+++ programmers/조이스틱.java
@@ -31,3 +31,48 @@ class Solution {
         return answer;
     }
 }
+
+/* 나중인데 한번 더 봐야게싿. 
+
+
+import java.util.*;
+class Solution {
+    public int solution(String name) {
+        int answer = 0;
+        char[] arr = name.toCharArray();
+        int sum =0;
+        for(int i=0; i<arr.length;i++){
+            int num =0;
+            if(arr[i]=='N') num=13;
+            else if(arr[i]>'N') num='N'-arr[i]+13;
+            else num = arr[i]-'A';
+                
+            sum+=num;
+        }
+        int move=(int)1e9;
+        for(int i=0;i<arr.length;i++){
+            int next =i+1;
+             while(next < arr.length && name.charAt(next) == 'A') next++;
+            move = Math.min(move, i + arr.length - next + 
+                            Math.min(i, arr.length - next));
+        }
+        
+        sum+=move;
+        return sum;
+    }
+    
+}
+
+
+/*
+INT N =13;
+A B C D E
+F G H I J
+K L M N O
+P Q R S T 
+U V W X Y 
+Z
+
+*/
+
+*/
