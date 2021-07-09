c1b595d6ff8230984b853f51775dc38bb8971f23
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-07-09 10:23:52 +0900

    Update 1417.java

--- BJ/1417.java
+++ BJ/1417.java
@@ -1,40 +1,3 @@
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.Scanner;
-
-public class Main {
-	
-	public static void main(String[] args) {
-		
-		Scanner sc = new Scanner(new InputStreamReader(System.in));
-
-		int n = sc.nextInt(); // 총 후보수 
-		int my = sc.nextInt();
-		int[] arr = new int[n-1];
-		
-		for(int i =0; i<n-1;i++) {
-			arr[i] =sc.nextInt();
-		}
-		int cnt =0;
-		while(true) {
-			Arrays.sort(arr);
-			int max = arr[n-2];
-			if( my>max) {
-				break;
-			}
-			arr[n-2]--;
-			my++;
-			cnt++;
-		}
-		
-		System.out.println(cnt);
-		
-	}
-}
-
-/* 어디서 틀리 걸까 . 
-
 package backJun;
 
 import java.io.IOException;
@@ -43,52 +6,41 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-	
+
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(new InputStreamReader(System.in));
 
-		int cnt =0;
-		int n = sc.nextInt(); // 총 후보수 
-		if( n==1) {
-			int my = sc.nextInt();
-		}else if(n==2){
-			int my = sc.nextInt();
-			int arr = sc.nextInt();
-			if(my<arr) {
-				cnt=arr-my+1;
-			}else if(my==arr) {
-				cnt=1;
-			}
+		int cnt = 0;
+		boolean flag = true;
+		int n = sc.nextInt(); // 총 후보수
 
-		}else {
+		int my = sc.nextInt();
+		if( n!=1) { // 왕.. 당연히 경쟁이라서 2명이상입력이 줄알았는데.. 혼자도 있네.
+			int[] arr = new int[n - 1];
 			
-			int my = sc.nextInt();
-			int[] arr = new int[n-1];
-			
-			for(int i =0; i<n-1;i++) {
-				arr[i] =sc.nextInt();
+			for (int i = 0; i < arr.length; i++) {
+				arr[i] = sc.nextInt();
 			}
-			boolean flag = true;
-			//if(arr.length==1) 
-			while(true) {
+			// if(arr.length==1)
+			while (true) {
 				Arrays.sort(arr);
-				int max = arr[arr.length-1];
-				if( my>max) {
+				int max = arr[arr.length - 1];
+				if (my > max) {
 					break;
 				}
-				arr[n-2]--;
+				arr[n - 2]--;
 				my++;
 				cnt++;
 			}
+			
 		}
-		
+
 		System.out.println(cnt);
-		
+
 	}
 }
 
 /*
 
 */
-
