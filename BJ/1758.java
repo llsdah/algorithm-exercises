a095ccfb66ff9f1b10bc6d9dbcecf4466465638f
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-07-13 10:49:07 +0900

    Update 1758.java

--- BJ/1758.java
+++ BJ/1758.java
@@ -14,10 +14,11 @@ public class Main {
 			arr[i] = sc.nextInt();
 		}
 		Arrays.sort(arr);
-		int cnt = 0;
+		long cnt = 0; //더하기 long 주의 
 		for(int i =c-1;i>=0;i--) {
 			int num = arr[i]-((c-i)-1);
-			cnt += (num >0 ? num : 0);
+			if(num<=0) break;
+			cnt += num;
 		}
 		System.out.println(cnt);
 	
