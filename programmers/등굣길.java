85d1e22d610eb3dbe28e5c7b1f39fc8ef6e85a7e
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-07-13 09:46:07 +0900

    Update 등굣길.java

--- programmers/등굣길.java
+++ programmers/등굣길.java
@@ -1,4 +1,23 @@
+class Solution {
+    public int solution(int m, int n, int[][] puddles) {
+        int answer = 0;
 
+        int[][] dp = new int[n+1][m+1];
+
+        for(int i = 0; i < puddles.length; i++) dp[puddles[i][1]][puddles[i][0]] = -1;
+
+        dp[1][1] = 1;
+        for(int i = 1; i <= n; i++) {
+            for(int j = 1; j <= m; j++) {
+                if(dp[i][j] == -1) continue;
+                if(i-1 > 0 && dp[i-1][j] != -1) dp[i][j] += dp[i-1][j]%1000000007;
+                if(j-1 > 0 && dp[i][j-1] != -1) dp[i][j] += dp[i][j-1]%1000000007;
+            }
+        }               
+        answer = dp[n][m]%1000000007;
+        return answer;
+    }
+}
 
 /* 일단 패스 수학 식으로 풀기 
 
