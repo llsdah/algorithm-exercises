726329175e82905333ba95333e163ec1ded8e068
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-07-13 10:01:41 +0900

    Update 도둑질.java

--- programmers/도둑질.java
+++ programmers/도둑질.java
@@ -1,4 +1,32 @@
 import java.util.*;
+class Solution {
+    public int solution(int[] money) {
+        int answer = 0;
+        int[] one = new int[money.length];
+        int[] two = new int[money.length];
+        // 그리디 많이 찾는거는 뒤에서 부터 찾으면 쉽다. 
+        // 2가지의 경우로 나누어서 풀기 
+        // 1. 마지막 것을 안훔치는 경우 
+        one[0] = money[0];
+        one[1] = money[1];
+        for(int i=2;i<money.length;i++){
+           one[i] = Math.max(one[i-1],money[i]+one[i-2]); 
+        }
+        
+        // 2. 마지막 것을 훔치는 경우 즉, 첫번쨰는 0 이다. 
+        two[1] = money[1];
+        for(int i=2;i<money.length;i++){
+           two[i] = Math.max(two[i-1],money[i]+two[i-2]); 
+        }
+        answer = Math.max(one[money.length-2],two[money.length-1]);
+        //여기 마지막 중요 마지막것 안 훔치니까 첫번쨰는 두번째 걸로 해야댕! 
+        return answer;
+    }
+}
+
+
+/*
+import java.util.*;
 
 class Solution {
     public int solution(int[] money) {
@@ -25,3 +53,4 @@ class Solution {
         return answer;
     }
 }
+*/
