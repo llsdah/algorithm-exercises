913c8f1202f1aca97f5c8f2ccbec28d0bcec768b
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-07-14 20:36:49 +0900

    Update 11501.java

--- BJ/11501.java
+++ BJ/11501.java
@@ -1,4 +1,45 @@
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
 
+public class Main {
+
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner(new InputStreamReader(System.in));
+		
+		int y = sc.nextInt();
+		for(int i =0; i<y;i++) {
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+			for(int k=0;k<n;k++) {
+				arr[k]=sc.nextInt();
+			}
+			int num = price(n,arr);
+			System.out.println(num);
+		}
+		
+	}
+
+	private static int price(int n, int[] arr) {
+		int sum =0;
+		int max =0;
+		for(int i=arr.length-1; i>=0;i--) {
+			if(max<=arr[i]) {
+				max = arr[i];
+			}else {
+				sum+=(max-arr[i]);
+			}
+			
+			
+		}
+		
+		return sum;
+	}
+	
+}
 
 
 
