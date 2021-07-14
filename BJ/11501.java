32cbec6e7b9cc403793eb57d2ec9d35b8af56c01
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-07-14 20:38:01 +0900

    Update 11501.java
    int-> long 은근히 많이 걸리네 확인 요망 백준

--- BJ/11501.java
+++ BJ/11501.java
@@ -1,3 +1,49 @@
+// 정렬 수정 완료 
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner(new InputStreamReader(System.in));
+		
+		int y = sc.nextInt();
+		for(int i =0; i<y;i++) {
+			int n = sc.nextInt();
+			int[] arr = new int[n];
+			for(int k=0;k<n;k++) {
+				arr[k]=sc.nextInt();
+			}
+			long num = price(n,arr);
+			System.out.println(num);
+		}
+		
+	}
+
+	private static long price(int n, int[] arr) {
+		long sum =0;
+		long max =0;
+		for(int i=arr.length-1; i>=0;i--) {
+			if(max<=arr[i]) {
+				max = arr[i];
+			}else {
+				sum+=(max-arr[i]);
+			}
+			
+			
+		}
+		
+		return sum;
+	}
+	
+}
+
+
+// 정렬 확인
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Collections;
