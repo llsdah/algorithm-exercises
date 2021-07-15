5582e2524473ef001a7e701250274a29a3bd26e5
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-07-15 14:14:14 +0900

    Update 순위.java

--- programmers/순위.java
+++ programmers/순위.java
@@ -58,3 +58,42 @@ class Solution {
 	}
 
 }
+
+
+/* 정렬 후 맞는지 확인 중.. 
+
+    ArrayList<ArrayList<Win>> list = new ArrayList<>();
+    public int solution(int n, int[][] results) {
+        int answer = 0;
+        // 0 번쨰가 승리 했다는 의미 . 
+        // 승패 2승 했다면 뒤에 두면은 있다는 의미; 진 사랑 이긴 사람 담을까?
+        Arrays.sort(results, new Comparator<int[]>(){
+            public int compare(int[] o1, int[] o2){
+                if(o1[0]==o2[0]) return o1[1]-o2[1];
+                return o1[0]-o2[0];
+            }
+        });
+        for(int i =0; i<=n;i++){
+            list.add(new ArrayList<>());
+        }
+        
+        for(int i=0; i<results.length;i++){
+            int win = results[i][0];
+            int lose = results[i][1];
+            
+        }
+        
+        return answer;
+    }
+}
+
+class Win{
+    String win;
+    String lose;
+    Win(String win, String lose){
+        this.win = win;
+        this.lose = lose;
+    }
+}
+
+*/
