9fabb52a4e26509e42226602b34d3222deb4454b
알고리즘공부 <77381506+llsdah@users.noreply.github.com>
2021-07-15 17:32:18 +0900

    Update 1461.java

--- BJ/1461.java
+++ BJ/1461.java
@@ -1,3 +1,66 @@
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {		
+		Scanner sc = new Scanner(new InputStreamReader(System.in));
+		
+		int n = sc.nextInt();
+		int book = sc.nextInt(); // 책 몇군?
+		int[] arr = new int[n];
+		for(int i =0; i<n;i++) {
+			arr[i]=sc.nextInt();
+		}
+		Arrays.sort(arr); // 기본 정렬 
+		PriorityQueue<Integer> plus = new PriorityQueue<Integer>((x,y)-> y-x);// 양수는 역정렬 합니다. 
+		PriorityQueue<Integer> mi = new PriorityQueue<Integer>();
+
+		for(int i =0; i <arr.length;i++) {
+			if(arr[i]>=0) plus.add(arr[i]);
+			else mi.add(arr[i]);
+		}
+		
+		int num =0; //기초값
+		int max =0;// 최대값은 안 움직임 한번 뺴줌
+		int sum =0; // 총합계 
+		// 양 수 
+		while(!plus.isEmpty()) {
+			for(int i=0; i<book;i++) { // 해당 갯수 만큼만 뺴고 넣고 
+				if(plus.isEmpty()) continue;
+				num = plus.poll();// 뺴면됩
+				// 역순으로 정렬해 놓은 상태 즉, 처음 방문이 제일 큰 숫자 
+				if( i ==0) {
+					sum+= num;// 어짜피 양수 
+					max = Math.max(max, num);// 젤 큰수 저장용
+				}
+			}
+		}// 양수 용
+		
+		// 음수 
+		while(!mi.isEmpty()) {
+			for(int i=0; i<book;i++) { // 해당 갯수 만큼만 뺴고 넣고 
+				if(mi.isEmpty()) continue;
+				num = mi.poll();// 뺴면됩
+				// 역순으로 정렬해 놓은 상태 즉, 처음 방문이 제일 큰 숫자 
+				if( i ==0) {
+					num = Math.abs(num); //음수 변환
+					sum+= num; 
+					max = Math.max(max, num);// 젤 큰수 저장용
+				}
+			}
+		}
+		
+		sum = sum *2 -max;
+		System.out.println(sum);
+		
+	}
+
+}
 /* 틀립.. 
 import java.io.InputStreamReader;
 import java.util.Arrays;
