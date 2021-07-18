df8a818492e3e66e70fb700c54df70484c7589ce
알고리즘공부(비공개) <77381506+llsdah@users.noreply.github.com>
2021-07-18 09:38:04 +0900

    Update 멀리뛰기.java

--- programmers/멀리뛰기.java
+++ programmers/멀리뛰기.java
@@ -1,5 +1,5 @@
 
-// 심각하게 많이 틀리네 
+// 문제를처음부터잘읽기.. 풀필요한 시간낭비 x 
 import java.util.*;
 class Solution {
     public long solution(int n) {
@@ -13,7 +13,7 @@ class Solution {
         arr[1] = 1;
         arr[2] = 2;
         for(int i=3;i<=n;i++){
-            arr[i] = arr[i-1]+arr[i-2];
+            arr[i] = (arr[i-1]+arr[i-2])%1234567;
         }
         
         answer = arr[n]%1234567;
