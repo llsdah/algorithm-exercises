ea07872143b1bb7ea9aa7eafe232be92b3a997dd
알고리즘공부(비공개) <77381506+llsdah@users.noreply.github.com>
2021-07-18 09:28:18 +0900

    Create 멀리뛰기.java

---
+++ programmers/멀리뛰기.java
@@ -0,0 +1,19 @@
+
+// 심각하게 많이 틀리네 
+import java.util.*;
+class Solution {
+    public long solution(int n) {
+        long answer = 0;
+        // 한번에 한칸 또는 두칸.
+        long[] arr = new long[n+1];
+        
+        arr[1] = 1;
+        arr[2] = 2;
+        for(int i=3;i<=n;i++){
+            arr[i] = arr[i-1]+arr[i-2];
+        }
+        
+        answer = arr[n];
+        return answer;
+    }
+}
