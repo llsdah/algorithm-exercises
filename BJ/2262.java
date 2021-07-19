09341e3af536f42200c0ebf40f144586e14e7fb7
알고리즘공부(비공개) <77381506+llsdah@users.noreply.github.com>
2021-07-19 18:31:59 +0900

    Update 2262.java
    list 사용법 숙지!

--- BJ/2262.java
+++ BJ/2262.java
@@ -1,4 +1,52 @@
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Scanner;
 
+public class Main {
+
+	
+	// 리스트의 indexOf(값) 어느 위치 반환
+	// 리스트 remove(index)  해당 위치 값 삭제 
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new InputStreamReader(System.in));
+		int max = sc.nextInt();
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i=0; i<max;i++) {
+			list.add(sc.nextInt());
+		}
+		
+		int cnt =0;
+		int low = max ; // 최대 값이 낮은 등수 입니다. 
+		while(list.size()>1) {
+			int pos = list.indexOf(low);// 꼴등 위치 반환
+			
+			if(pos==0) {
+				cnt+= list.get(pos) - list.get(pos+1); 
+			}else if( pos == list.size()-1) { // 맨끝에 있으면 ?
+				cnt += list.get(pos) - list.get(pos-1); // 큰수 - 작은수!!
+			}else {
+				cnt+= Math.min( list.get(pos) - list.get(pos-1),
+						list.get(pos) - list.get(pos+1));
+			}
+			list.remove(pos); //삭제만 하면됨하하하 왜냐하면. 이긴사람은 계속 남아있으니까
+			low--;// 
+		}
+		System.out.println();
+		System.out.println(cnt);
+		
+		System.out.println();
+		ArrayList<Integer> temp = new ArrayList<>();
+		for(int i=0; i<10;i++) {
+			temp.add(i);
+		}
+		System.out.println("5번위 의치"+ temp.indexOf(5));
+		temp.remove(3);
+		System.out.println("3번쨰 삭제 후 5번위 의치"+ temp.indexOf(5));
+		
+		
+	}
+
+}
 
 
 //
