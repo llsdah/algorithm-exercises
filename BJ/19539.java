f3872e0025aaf0548c7aa662e9532ffff7cdd9f4
알고리즘공부(비공개) <77381506+llsdah@users.noreply.github.com>
2021-07-20 15:33:41 +0900

    Update 19539.java

--- BJ/19539.java
+++ BJ/19539.java
@@ -13,6 +13,37 @@ public class Main {
 		for(int i=0;i<n;i++) {
 			tree[i]=sc.nextInt()%3;// 최대가 3이니까.
 		}
+		int sum=0;
+		int odd =0;
+		for(int i=0;i<n;i++) {
+			sum+=tree[i];
+			if(tree[i]%2==1) odd++;
+		}
+		
+		if(sum%3==0&&odd<=sum/3 ) {
+			System.out.println("YES");
+		}else {
+			System.out.println("NO");
+		}
+		
+		
+		// 물뿌리개 +1 ? +2
+		
+		
+		
+	}
+}
+// 수학 식 재정의
+public class Main {
+
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new InputStreamReader(System.in));
+		int n = sc.nextInt();// 탭 해야댈 수 
+		int[] tree =new int[n];
+		for(int i=0;i<n;i++) {
+			tree[i]=sc.nextInt()%3;// 최대가 3이니까.
+		}
 		System.out.println(Arrays.toString(tree));
 		boolean flag =false;
 		if(n==1&&(tree[0]==1||tree[0]==2||tree[0]==3)) {
