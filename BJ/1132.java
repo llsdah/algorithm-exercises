73a712dca9fa72e1f487c411bad94f2c8909fbac
알고리즘공부(비공개) <77381506+llsdah@users.noreply.github.com>
2021-07-26 18:55:59 +0900

    Update 1132.java

--- BJ/1132.java
+++ BJ/1132.java
@@ -1,11 +1,67 @@
+import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
-import java.util.HashMap;
+import java.util.Collections;
 import java.util.Scanner;
 
 public class Main {
 
-	
+	static String arr[];
+	static Long longs[] = new Long[10];
+	static boolean head[] = new boolean[10];
+
+	public static void main(String[] args) throws IOException {
+		Scanner sc = new Scanner(new InputStreamReader(System.in));
+		int n = sc.nextInt();
+		String[] str = new String[n];
+		for (int i = 0; i < n; i++) {
+			str[i] = sc.next();
+
+		}
+		check(str);
+
+		long sum = 0, value = 9;
+		for (Long aLong : longs) {
+			sum += aLong * value--;
+		}
+		System.out.println(sum);
+
+	}
+
+	private static void check(String[] str) {
+
+		long inp[] = new long[10];
+
+		for(int k=0;k<str.length;k++) {
+			char[] arr = str[k].toCharArray();
+			int len = arr.length;
+			long d = 1;
+			for (int i = len - 1; i >= 0; i--, d *= 10) {
+				inp[arr[i] - 'A'] += d;
+			}
+			head[arr[0] - 'A'] = true; // 맨앞에 나온것들 !!! 1개는 안나온다 마지막 조건.. 
+		}
+
+		// 0을 사용하는 경우와 안사용하는 경우 
+		long min = Long.MAX_VALUE;
+		int last = 0;
+		for (int i = 0; i < 10; i++) {
+			if (!head[i] && inp[i] < min) {
+				last = i;
+				min = inp[i];
+			}
+		}
+		inp[last] = 0;
+		longs = Arrays.stream(inp).boxed().toArray(Long[]::new);
+		Arrays.sort(longs, Collections.reverseOrder());
+	}
+
+}
+
+
+/// 마지막 조건 이 충족... 한개는 맨앞에안온다..
+public class Main {
 	
 	static HashMap<Character, Long> map = new HashMap<>();
 	static long no =-1;
