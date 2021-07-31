3e68d58fafd4b16ffa038cc7d8fa9a46d5829508
알고리즘공부(비공개) <77381506+llsdah@users.noreply.github.com>
2021-07-31 12:56:01 +0900

    Update 14939.java

--- BJ/14939.java
+++ BJ/14939.java
@@ -1,3 +1,89 @@
+
+// 정답자분 정답 보면서 수정한ㄱㄴ데 .. 흠. 
+public class Main {
+	// 전구 불 끄기 
+	// 4 3 2 1 로 돌면서 뺴면 안되띾? 10 * 4 400번이면 되럮같은데 
+	static boolean[][] map = new boolean[10][10];
+	static int cnt =(int)1e9;
+	static int[] d = {0,0,1,-1};//방향 회전용
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(new InputStreamReader(System.in));
+	
+		for(int i =0; i < 10;i++) {
+			String str = sc.next();
+			char[] temp = str.toCharArray();
+			for(int k=0; k<10;k++) {
+				if(temp[k]=='O') map[i][k] =true;
+			}
+			
+		}
+		
+		// 1줄 당 불을 키고 안키고!! 2^ 10 이다.
+		
+		for(int i=0;i<1024;i++) {
+			int num = Integer.bitCount(i);// 비트 카운드?
+			boolean[][] temp = new boolean[10][10];
+			
+			for(int k=0;k<10;k++) {
+				temp[k] = map[k].clone(); // 깊은 복사 실시 
+			}
+			
+			for(int k =0; k<10;k++) {
+				if( (i&(1<<k))!=0) {
+					temp[0][k]^=true; // 반대로 뒤집기
+				
+					for( int h=0;h<3;h++) {
+						int X = d[h];
+						int Y = d[3-h];// 대칭된거
+						
+						if(X<0||Y<0||Y>=10) continue; //넘어가면 
+						temp[X][Y]^=true;// ㅈㄴ구 불 바꾸기 
+					}
+				
+				}// if 문
+			}// 전구 불빠구는 용고
+			
+			check(1,num,temp);
+			
+		}
+
+		System.out.println();
+		System.out.println(cnt);
+	}
+
+	private static void check(int col, int num, boolean[][] temp) {
+
+		
+		if(col==10) {
+			boolean flag = false;
+			for(int i=0; i<10;i++) {
+				if(temp[9][i]) flag = true;// 10번째 니까 그 전부분만 확인 중 불 켜짐?
+			}
+			if(!flag) cnt = Math.min(num, cnt); // 다 꺼질 겨웅만
+			return;
+		}
+		
+		for(int i=0; i<10;i++) {
+			if( temp [col-1][i]) {
+				temp[col][i]^=true;
+				num++;
+				
+				for(int k=0;k<3;k++) {
+					int X =d[k]+col;// 해당 열
+					int Y = d[3-k]+i;//헹
+					if(X<0||Y<0||X>=10||Y>=10) continue; //넘어가면 
+					temp[X][Y]^=true;// ㅈㄴ구 불 바꾸기 
+				}
+			}
+		}
+		
+		check(col+1,num,temp);
+		
+	}
+	
+}
+
 public class Main {
 	// 전구 불 끄기 
 	// 4 3 2 1 로 돌면서 뺴면 안되띾? 10 * 4 400번이면 되럮같은데 
