da68bb19d379e8dd8662d8a0c6241f891b70bfad
llsdah <77381506+llsdah@users.noreply.github.com>
2023-09-29 16:33:19 +0900

    알고리즘 레포 정리2

---
+++ build.gradle.kts
@@ -0,0 +1,24 @@
+plugins {
+    id("java")
+}
+
+group = "org.example"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.9.1"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.15.2")
+
+    implementation("org.slf4j:slf4j-api:2.0.9")
+
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
---
+++ src/main/java/org/example/Q4Main.java
@@ -0,0 +1,17 @@
+package org.example;
+
+import java.util.List;
+
+public class Q4Main {
+
+    private List<Q4Sub> items;
+
+
+    public void setItems(List<Q4Sub> items) {
+        this.items = items;
+    }
+
+    public List<Q4Sub> getItems() {
+        return items;
+    }
+}
